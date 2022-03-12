33f5fbd427b41a8397467325bdb2c9c8be778ebc
Piyush Bhangale <bhangalepiyush@gmail.com>
2022-03-12 12:48:09 +0530

    create user admin database

--- backend/src/routes/admin.ts
+++ backend/src/routes/admin.ts
@@ -1,20 +1,62 @@
 import { Router } from "express";
+import { CourtRequest } from "../../typings/custom";
 import { createUser } from "../lib/auth";
+import { addUser } from "../lib/database";
+import prisma from "../lib/prisma";
 
 const router = Router();
 
-router.post("/add-user", async (req, res) => {
-  const { email, password } = req.body;
+router.post("/add-user", async (req: CourtRequest, res) => {
+  if (!req.user) {
+    res.status(401).json({
+      error: true,
+      message: "Not logged in",
+    });
+  }
 
-  const user = await createUser(email, password);
+  const { email, password, role } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+      isMainAdmin: true,
+      role: "MAIN_ADMIN",
+    },
+  });
 
   if (!user) {
+    res.status(401).json({
+      error: true,
+      message: "Unauthorized",
+    });
+  }
+
+  const createdUser = await createUser(email, password);
+
+  if (!createdUser) {
     res.json({
       error: true,
       message: "User not created",
     });
     return;
   }
+
+  const databaseRoleUser = await addUser(email, role);
+
+  if (!databaseRoleUser) {
+    res.json({
+      error: true,
+      message: "User not created in database",
+    });
+    return;
+  }
+
+  res.json({
+    createdUser,
+    databaseRoleUser,
+  });
+
+  return;
 });
 
 export default router;
