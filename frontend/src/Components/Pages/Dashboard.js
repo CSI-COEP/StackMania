6623ca763032b6414696f0d58b3956e835466f24
Piyush Bhangale <bhangalepiyush@gmail.com>
2022-03-13 08:20:10 +0530

    dyanmic data

--- frontend/src/Components/Pages/Dashboard.js
+++ frontend/src/Components/Pages/Dashboard.js
@@ -53,22 +53,22 @@ const Dashboard = () => {
       <div className="dashboard-div">
         <div className="card-all">
           <div className="card" data-aos="flip-up">
-            <h1>98</h1>
-            <p>Cases Pending</p>
+            <h1>{(data && data.openData && data.openData.length) ? data.openData.length : "0"}</h1>
+            <p>Cases Ongoing</p>
           </div>
         </div>
 
         <div className="card-all" data-aos="flip-up">
           <div className="card-purple">
-            <h1>3265</h1>
+            <h1>{(data && data.allData && data.allData.length) ? data.allData.length : "0"}</h1>
             <p>Cases Listed</p>
           </div>
         </div>
 
         <div className="card-all" data-aos="flip-up">
           <div className="card-green">
-            <h1>235</h1>
-            <p>Disposed Cases</p>
+            <h1>{(data && data.closedData && data.closedData.length) ? data.closedData.length : "0"}</h1>
+            <p>Cases Closed</p>
           </div>
         </div>
       </div>
