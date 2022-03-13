4a8874d286b2bf6854d3ab7e7242196c0ad828da
Piyush Bhangale <bhangalepiyush@gmail.com>
2022-03-13 08:30:47 +0530

    states

--- frontend/src/Components/Pages/PoliceDashboard.js
+++ frontend/src/Components/Pages/PoliceDashboard.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Web3Storage } from 'web3.storage'
 
 const client = new Web3Storage({ token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDREOUU2Mjg3ZEVBRGY5MDQ3ZDAwMGZGOGQ1MzI1OWM0MGM4MzQ3QkIiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDcxMzczNzIyNTEsIm5hbWUiOiJEaWdpQ291cnQifQ.ZApsW-5xdLSSWkzlm47TDofglDG8vjZiYzp2TlJhlBo" })
 
 const PoliceDashboard = () => {
+  const [userMail, setUserMail] = useState("")
+  const [lawyerMail, setLawyerMail] = useState("")
+  const [bookedUnder, setBokoedUnder] = useState("")
+
   const onFileChange = async (event) => {
     const files = event.target.files;
     if (!files.length) return
@@ -24,22 +28,22 @@ const PoliceDashboard = () => {
   return (
     <div className='generate-case-div'>
       <div>
-      <h2>Generate Case Instructions : </h2>
-      <li>Fill all the required details in case-form official letterhead of police station</li>
-      <li>Also fill the form given on the right side</li>
-      <li>File should be uploaded in ".pdf",".doc" format</li>
-      <li>Upload it Below</li>
-      <input onChange={onFileChange} className='fileinput' type="file" placeholder='Upload Filled Form Here...'></input>
+        <h2>Generate Case Instructions : </h2>
+        <li>Fill all the required details in case-form official letterhead of police station</li>
+        <li>Also fill the form given on the right side</li>
+        <li>File should be uploaded in ".pdf",".doc" format</li>
+        <li>Upload it Below</li>
+        <input onChange={onFileChange} className='fileinput' type="file" placeholder='Upload Filled Form Here...'></input>
       </div>
 
       <div className='case-form'>
         <h3>Case Form</h3>
-        <input type="text" placeholder="Enter User mailID"></input>
-        <input type="text" placeholder="Enter Lawyer mailID"></input>
-        <input type="text" placeholder="Booked Under Section"></input>
+        <input value={userMail} onChange={event => setUserMail(event.target.value)} type="text" placeholder="Enter User mailID"></input>
+        <input value={lawyerMail} onChange={event => setLawyerMail(event.target.value)} type="text" placeholder="Enter Lawyer mailID"></input>
+        <input value={bookedUnder} onChange={event => setBokoedUnder(event.target.value)} type="text" placeholder="Booked Under Section"></input>
         <button className="signup-btn">
-           SUBMIT
-          </button>
+          SUBMIT
+        </button>
       </div>
     </div>
   )
